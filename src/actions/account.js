1c5223de95be2efa986dc357bd9b2b0699b9bd0e
Scott <prescottprue@users.noreply.github.com>
2016-08-10 23:56:35 -0700

    Clone capability Login/Signup (#2)
    * Login and signup actions modified for clone on signup
    * Clone on login and signup working
    * eslint and babel versions updated
    * version v0.1.5 of devshare

--- src/actions/account.js
+++ src/actions/account.js
@@ -3,94 +3,95 @@ export const LOGIN_REQUEST = 'LOGIN_REQUEST'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAILURE = 'LOGIN_FAILURE'
 
-export function login (username, password) {
-  return {
+export const login = (username, password, project) => (
+  {
     [CALL_DEVSHARE]: {
       types: [ LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE ],
       method: 'login',
-      methodArgs: [ username, password ]
+      methodArgs: Array.prototype.slice.call(arguments)
     }
   }
-}
+)
 
 export const PROVIDER_REQUEST = 'PROVIDER_REQUEST'
 export const PROVIDER_SUCCESS = 'PROVIDER_SUCCESS'
 export const PROVIDER_FAILURE = 'PROVIDER_FAILURE'
 
-export function authWithProvider (provider) {
-  return {
+export const authWithProvider = (provider) => (
+  {
     [CALL_DEVSHARE]: {
       types: [ PROVIDER_REQUEST, PROVIDER_SUCCESS, PROVIDER_FAILURE ],
       method: 'authWithProvider',
       methodArgs: provider
     }
   }
-}
+)
 
 export const SIGNUP_REQUEST = 'SIGNUP_REQUEST'
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS'
 export const SIGNUP_FAILURE = 'SIGNUP_FAILURE'
 
-export function signup (signupData) {
-  return {
+export const signup = (signupData, project) => (
+  {
     [CALL_DEVSHARE]: {
       types: [ SIGNUP_REQUEST, SIGNUP_SUCCESS, SIGNUP_FAILURE ],
       method: 'signup',
-      methodArgs: signupData
+      methodArgs: Array.prototype.slice.call(arguments)
     }
   }
-}
+)
 
 export const LOGOUT_REQUEST = 'LOGOUT_REQUEST'
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
 export const LOGOUT_FAILURE = 'LOGOUT_FAILURE'
 
-export function logout () {
-  return {
+export const logout = () => (
+  {
     [CALL_DEVSHARE]: {
       types: [ LOGOUT_REQUEST, LOGOUT_SUCCESS, LOGOUT_FAILURE ],
       method: 'logout'
     }
   }
-}
+)
+
 export const RECOVER_REQUEST = 'RECOVER_REQUEST'
 export const RECOVER_SUCCESS = 'RECOVER_SUCCESS'
 export const RECOVER_FAILURE = 'RECOVER_FAILURE'
 
-export function recover (recoverData) {
-  return {
+export const recover = (recoverData) => (
+  {
     [CALL_DEVSHARE]: {
       types: [ RECOVER_REQUEST, RECOVER_SUCCESS, RECOVER_FAILURE ],
       method: 'recoverAccount',
       methodArgs: recoverData
     }
   }
-}
+)
 
 export const UPLOAD_AVATAR_REQUEST = 'UPLOAD_AVATAR_REQUEST'
 export const UPLOAD_AVATAR_SUCCESS = 'UPLOAD_AVATAR_SUCCESS'
 export const UPLOAD_AVATAR_FAILURE = 'UPLOAD_AVATAR_FAILURE'
 
-export function uploadAvatar (methodArgs) {
-  return {
+export const uploadAvatar = (methodArgs) => (
+  {
     [CALL_DEVSHARE]: {
       types: [ UPLOAD_AVATAR_REQUEST, UPLOAD_AVATAR_SUCCESS, UPLOAD_AVATAR_FAILURE ],
       method: 'uploadAvatar',
       methodArgs
     }
   }
-}
+)
 
 export const UPDATE_ACCOUNT_REQUEST = 'UPDATE_ACCOUNT_REQUEST'
 export const UPDATE_ACCOUNT_SUCCESS = 'UPDATE_ACCOUNT_SUCCESS'
 export const UPDATE_ACCOUNT_FAILURE = 'UPDATE_ACCOUNT_FAILURE'
 
-export function updateAccount (methodArgs) {
-  return {
+export const updateAccount = (methodArgs) => (
+  {
     [CALL_DEVSHARE]: {
       types: [ UPDATE_ACCOUNT_REQUEST, UPDATE_ACCOUNT_SUCCESS, UPDATE_ACCOUNT_FAILURE ],
       method: 'updateAccount',
       methodArgs
     }
   }
-}
+)
